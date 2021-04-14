6e5e17501f5d86632ed576b4938f6adaa455fa8c
Johanne Bognøy <johabog@stud.ntnu.no>
2021-04-14 12:05:57 +0200

    Added a serverless deploy script

---
+++ ci/deploy_no_server.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+cd ig-coder
+git pull
+
+# This version of deploy.sh skips creating the server.
+
+# Cleanup
+rm -rf /var/www/app
+# Re-create directory structure on the web-root
+mkdir -p /var/www/app
+mkdir -p /var/www/app/server
+mkdir -p /var/www/app/client
+
+# Generate nginx config file to sites-enabled folder
+set -e
+echo "client_max_body_size 0;" > /etc/nginx/conf.d/upload.conf
+
+# Generate Nginx config
+echo "server {
+         listen 80;
+
+         location / {
+             alias /var/www/app/client/;
+             try_files \$uri \$uri/ /index.html;
+         }
+      }" > /etc/nginx/sites-enabled/igcoder
+exec "$@"
+
+chmod -R 777 /var/www/app/.*
+
+# Start build for the client app
+cd client
+npm install
+npm run build:test
+cp -a /home/ubuntu/ig-coder/client/build/. /var/www/app/client -rf
+
+# Restart services
+systemctl daemon-reload
+systemctl restart nginx
+echo 'Deployment done!\r\r\r'
